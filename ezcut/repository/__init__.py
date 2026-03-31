6767001aa2db25669681fe3101fcf3a7a6d1f32f
okccc5 <okccc5@naver.com>
2026-03-31 12:05:36 +0900

    feat: repository history 추가

--- ezcut/repository/__init__.py
+++ ezcut/repository/__init__.py
@@ -1,3 +1,4 @@
 from .config import AppConfig, ConfigRepository
+from .history import HistoryEntry, HistoryRepository
 
-__all__ = ["AppConfig", "ConfigRepository"]
+__all__ = ["AppConfig", "ConfigRepository", "HistoryEntry", "HistoryRepository"]
